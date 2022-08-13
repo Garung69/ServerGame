6ca04789da27e1cb3c820fd9731ce28ff36b9114
Garung69 <87683575+Garung69@users.noreply.github.com>
2022-08-13 20:33:24 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.use(express.static('public'));
+app.use("/scripts", express.static(__dirname + "/node_modules/web3.js-browser/build/"));
+var bodyPaser = require("body-parser");
+app.use(bodyPaser.urlencoded({ extended: true }));
+var server = require("http").Server(app);
+// var io = require("socket.io")(server);
+// app.io = io;
+const port = process.env.PORT || 3000;
+server.listen(port);
+
+app.get('/', function (req, res) {
+    res.render("home");
+})
+
+app.get('/test', function (req, res) {
+    res.render("test");
+})
+
+app.get('/nftdetail/:id', function (req, res) {
+    res.end(JSON.stringify((
+        {
+            "name": "NFT for test #" + req.params.id,
+            "description": "This is one items in the list of the Test Game Blockchain Collection",
+            "image_url": "http://" + req.params.id + ".png",
+            "tokenId": req.params.id,
+        })));
+})
